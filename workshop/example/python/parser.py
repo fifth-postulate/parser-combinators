d4fe199a76d03e8466410e70edcef1ca1b147b03
Daan van Berkel <daan.van.berkel@alliander.com>
2021-03-06 11:52:54 +0100

    Allow multiple parsers to be joined after the first

--- workshop/example/python/parser.py
+++ workshop/example/python/parser.py
@@ -1,6 +1,6 @@
 class Parser:
-    def andThen(self, other):
-        return Consecutive(self, other)
+    def andThen(self, *others):
+        return Consecutive([self] + list(others))
 
 class Character(Parser):
     def __init__(self, target):
@@ -16,12 +16,18 @@ def character(target):
     return Character(target)
 
 class Consecutive(Parser):
-    def __init__(self, first, second):
-        self.first = first
-        self.second = second
+    def __init__(self, parsers):
+        self.parsers = parsers
     
     def parse(self, input):
+        return parse_with(self.parsers, input)
         return [([r1, r2], rest) for (r1, intermediate) in self.first.parse(input) for (r2, rest) in self.second.parse(intermediate)]
+
+def parse_with(parsers, input):
+    if not parsers:
+        return [([], input)]
+    else:
+        return [([r1] + results, remaining) for (r1, intermediate) in parsers[0].parse(input) for (results, remaining) in parse_with(parsers[1:], intermediate)]
 
 if __name__ == '__main__':
     assert character('A').parse('ABC') == [('A', 'BC')]
@@ -31,7 +37,9 @@ if __name__ == '__main__':
 
     A = character('A')
     B = character('B')
+    C = character('C')
 
     assert A.andThen(B).parse('ABC') == [(['A', 'B'], 'C')]
     assert A.andThen(B).parse('ABCD') == [(['A', 'B'], 'CD')]
     assert A.andThen(B).parse('aBC') == []
+    assert A.andThen(B, C).parse('ABC') == [(['A', 'B', 'C'], '')]
