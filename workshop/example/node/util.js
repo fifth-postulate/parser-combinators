b882c90e4358ea635c2b682f59fba02cf4c14248
Daan van Berkel <daan.van.berkel@alliander.com>
2021-06-10 08:57:21 +0200

    introduce consecutive

--- workshop/example/node/util.js
+++ workshop/example/node/util.js
@@ -15,8 +15,22 @@ greedy1 = module.exports.greedy1 = function greedy1(p) {
     return first(Parser.many1(p))
 }
 
+consecutive = module.exports.consecutive = function consecutive(parsers) {
+    if (parsers.length == 0) {
+        return Parser.succeed([])
+    } else {
+        return Parser.map(Parser.and(parsers[0], consecutive(parsers.slice(1))), function(result){
+            return [result[0]].concat(result[1])
+        })
+    }
+}
+
 A = Parser.character('A')
+B = Parser.character('B')
+C = Parser.character('C')
 
 assert.deepEqual(first(many(A))("AAAB"), [[['A', 'A', 'A'], "B"]])
 assert.deepEqual(greedy(A)("AAAB"), [[['A', 'A', 'A'], "B"]])
 assert.deepEqual(greedy1(A)("B"), [])
+
+assert.deepEqual(consecutive([A, B, C])("ABCD"), [[['A', 'B', 'C'], "D"]])
